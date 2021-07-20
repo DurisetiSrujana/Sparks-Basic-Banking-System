45761dcee1ed507ca7ea9e4ac57c1fcfc57ca935
Srujana Duriseti <83491632+DurisetiSrujana@users.noreply.github.com>
2021-07-20 19:33:44 +0530

    Delete database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$con=mysqli_connect("localhost","root","");
-    $db=mysqli_select_db($con,"bankdb");
-    if(!$con){
-        die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-    }
-
-?>
